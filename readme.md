eb244f595bc5761e5e15d83d5335ceea373786fe
KarenHuang0421 <a0988423161@gmail.com>
2021-09-16 01:03:41 +0800

    add toolbar in pages

--- readme.md
+++ readme.md
@@ -14,12 +14,14 @@
 
 ## Tables
 
-|page   | Params          | represent     | text       |
+|Page   | Params          | Represent     | Text       |
 |-------|---------------- |:-------------:|:----------:|
-|General| page 1          | 首頁           | 首頁       |
-|       | page 2          | 個人的行程庫    | 行程       |
-|       | page 3          | 個人專頁/設定   | 個人       |
-|home   | Content Type 1  | 其他人的行程分享| 最新旅遊分享 |
+|General| Page 1          | 首頁           | 首頁       |
+|       | Page 2          | 個人的行程庫    | 行程       |
+|       | Page 3          | 個人專頁/設定   | 個人       |
+|Home   | Content Type 1  | 其他人的行程分享| 最新旅遊分享 |
 |       | Content Type 2  | 歸類的活動     | 更多       |
 |       | Category 1      | 單一地點的活動  | 活動       |
 |       | Category 2      | 鄰近的活動     | 鄰近的活動  |
+|Mytrip | Tab 1           | 收藏/建立的行程 | 行程       |
+|       | Tab 2           | 收藏的活動     | 活動       |
--- screen/MyTrips.js
+++ screen/MyTrips.js
@@ -1,27 +1,51 @@
-import React, { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Column, Row, StyleText } from "../component/components";
 import { Button, Card } from "react-native-elements";
 
-const MyTrips = () => {
-    const navigation = useNavigation();
-    const [cards, setCards] = useState([{}]);
+const ToolBar = ({ onPress }) => {
+    return(
+        <Row style={styles.segmentedControl}>
+            <TouchableOpacity
+                style={{width: '50%'}}
+                onPress={onPress}>
+                <Column 
+                    flex={1} 
+                    h="center" 
+                    style={styles.segmented}>tab 1
+                </Column>
+            </TouchableOpacity>
+            <Column style={{width:1, height:'100%', background:'#2089dc' }}  />
+            <TouchableOpacity
+                style={{width: '50%'}}
+                onPress={onPress}>
+                <Column 
+                    flex={1} 
+                    h="center" 
+                    style={styles.segmented}>tab 2
+                </Column>
+            </TouchableOpacity>
+        </Row>
+    )
+}
 
-    return (
+const Content = ({ add, data, onPress, type }) => {
+    return(
         <Column h="center" v="center">
-            {cards.map((item, key) => (
+            {data.map((item, key) => (
                 <TouchableOpacity
                     key={key}
-                    style={{ width: "100%" }}
-                    onPress={() =>
-                        navigation.navigate("TimelineScreen", {
-                            name: `CARD ${key + 1} 行程表`,
-                            page: 0,
-                        })
-                    }
+                    style={{ width: "90%" }}
+                    // onPress={() =>
+                    //     navigation.navigate("TimelineScreen", {
+                    //         name: `CARD ${key + 1} 行程表`,
+                    //         page: 0,
+                    //     })
+                    // }
+                    onPress={() => onPress(key)}
                 >
-                    <Card containerStyle={{ width: "90%" }}>
+                    <Card containerStyle={{ marginHorizontal: 0, width: "100%" }}>
                         <Card.Title>CARD {key + 1}</Card.Title>
                         <Card.Divider />
                         <Column>
@@ -30,19 +54,69 @@ const MyTrips = () => {
                     </Card>
                 </TouchableOpacity>
             ))}
-            <Button
+            {!type && <Button
                 title="ADD CARD"
                 containerStyle={{ width: "90%", marginTop: 20 }}
-                onPress={() =>
-                    navigation.navigate("Schedule", { name: "", page: 0 })
-                }
-            />
+                onPress={add}
+                // onPress={() =>
+                //     navigation.navigate("Schedule", { name: "", page: 0 })
+                // }
+            />}
             {/* <Button
                 title="ADD CARD"
                 containerStyle={{ width: "90%", marginTop: 20 }}
                 onPress={() => setCards(cards.concat({}))}
             /> */}
         </Column>
+    )
+}
+
+const MyTrips = () => {
+    const navigation = useNavigation();
+    const [tab, setTab] = useState(false);
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+
+        if(!tab){
+            setCards([{}, {}, {}])
+        }else{
+            setCards([])
+        }
+    }, [tab])
+
+    return (
+        <Column>
+            <ToolBar onPress={() => setTab(!tab)} />
+            <Content 
+                type={tab}
+                data={cards}
+                add={() => navigation.navigate("Schedule", { name: "", page: 0 })}
+                onPress={(key) =>
+                    navigation.navigate("TimelineScreen", {
+                        name: `CARD ${key + 1} 行程表`,
+                        page: 0,
+                    })} />
+        </Column>
     );
-};
+}
 export default MyTrips;
+
+const styles = StyleSheet.create({
+    box: {
+        width: 250,
+        height: 100
+    },
+    segmentedControl: {
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: '#2089dc',
+        margin: 'auto',
+        marginTop: 16,
+        width: '90%'
+    },
+    segmented: {
+      color: '#2089dc',
+      paddingVertical: 5
+    }
+});
